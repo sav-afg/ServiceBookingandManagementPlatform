503e1acf9eefaf87f8a5dfd026dbde53928331b2
sav-afg <s_hakimzada@hotmail.com>
2026-02-08 13:52:20 +0000

    Initial commit

--- WasmClient/obj/Release/net10.0/WasmClient.AssemblyInfo.cs
+++ WasmClient/obj/Release/net10.0/WasmClient.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WasmClient")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0122eb16a54d2fed8831c2d13ddd0aa69ccd38dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WasmClient")]
